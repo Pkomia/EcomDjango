4177fc21f1ab94930931f4ffb56937c3d6f78927
Pradeep <pradeep876596@gmail.com>
2023-01-13 15:55:42 +0530

    initial commit

---
+++ myshop/urls.py
@@ -0,0 +1,10 @@
+
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name="index"),
+    path('about/', views.about, name= "about")
+    # path('/about', views.about name="about")
+]
